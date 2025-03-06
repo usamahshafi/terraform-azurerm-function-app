91d31807434ad44961cbaf991d0b88c252b6d3d6
Your Name <you@example.com>
2025-03-06 12:56:13 -0800

    removed backend

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-backend"  
-    storage_account_name = "backendstorage121"                      
-    container_name       = "tfstate"                       
-    key                  = "dev.terraform.tfstate"        
-  }
-}
