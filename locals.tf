b84712386c618b93c151e416790c5ffe0dbeffbe
Your Name <you@example.com>
2025-03-12 10:59:52 -0700

    test

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   prefix = var.prefix
 
   resource_group_name = "${local.prefix}-rg" 
-  storage_account_name = lower("${local.prefix}13671283sa")
+  storage_account_name = lower("${local.prefix}13283sa")
   app_service_plan_name    = "${local.prefix}-asp"
-  function_app_name    = "${local.prefix}-fa"
+  function_app_name    = "${local.prefix}15533-fa"
 }
